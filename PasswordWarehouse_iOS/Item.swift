05dfedbf34a61fd06a62f57f1362af42dea7375e
Chandler Cheng <dc9909@hotmail.com>
2023-11-17 08:34:27 +1300

    Initial Commit

---
+++ PasswordWarehouse_iOS/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  PasswordWarehouse_iOS
+//
+//  Created by ChandlerC on 17/11/23.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
