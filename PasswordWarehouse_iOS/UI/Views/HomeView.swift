9caec1cc49e64025d6f054ba7cdb5104c300fa2e
Chandler Cheng <dc9909@hotmail.com>
2023-11-19 12:00:36 +1300

    Add searchable into HomeView

--- PasswordWarehouse_iOS/UI/Views/HomeView.swift
+++ PasswordWarehouse_iOS/UI/Views/HomeView.swift
@@ -12,48 +12,53 @@ struct HomeView: View {
     @State private var username: String = ""
     @State private var password: String = ""
     
+    @State private var searchText = ""
     
     var body: some View {
-        VStack(spacing: 16) {
-            Spacer()
+        NavigationView {
+            VStack(spacing: 16) {
+                LabeledContent {
+                    TextField("", text: $website)
+                        .textFieldStyle(.roundedBorder)
+                } label: {
+                    Label("Website", systemImage: "globe")
+                }
+                .labeledContentStyle(.vertical)
 
-            LabeledContent {
-                TextField("", text: $website)
-                    .textFieldStyle(.roundedBorder)
-            } label: {
-                Label("Website", systemImage: "hammer")
-            }
-            .labeledContentStyle(.vertical)
+                LabeledContent {
+                    TextField("", text: $username)
+                        .disableAutocorrection(true)
+                        .textFieldStyle(.roundedBorder)
+                        .textInputAutocapitalization(.never)
+                } label: {
+                    Label("Username (email)", systemImage: "envelope")
+                }
+                .labeledContentStyle(.vertical)
 
-            LabeledContent {
-                TextField("", text: $username)
-                    .disableAutocorrection(true)
-                    .textFieldStyle(.roundedBorder)
-                    .textInputAutocapitalization(.never)
-            } label: {
-                Label("Username (email)", systemImage: "hammer")
+                LabeledContent {
+                    TextField("", text: $password)
+                        .disableAutocorrection(true)
+                        .textFieldStyle(.roundedBorder)
+                        .textInputAutocapitalization(.never)
+                } label: {
+                    Label("Password", systemImage: "rectangle.and.pencil.and.ellipsis")
+                }
+                .labeledContentStyle(.vertical)
+                
+                Spacer().frame(height: 20)
+                
+                Button(action: {}) {
+                    Label("Store", systemImage: "square.and.arrow.down")
+                }
             }
-            .labeledContentStyle(.vertical)
-
-            LabeledContent {
-                TextField("", text: $password)
-                    .disableAutocorrection(true)
-                    .textFieldStyle(.roundedBorder)
-                    .textInputAutocapitalization(.never)
-            } label: {
-                Label("Password", systemImage: "hammer")
-            }
-            .labeledContentStyle(.vertical)
-            
-            Spacer()
-            
-            Button(action: {}) {
-                Label("Store", systemImage: "square.and.arrow.down")
-            }
-
-            Spacer()
+            .padding()
+            .navigationBarTitleDisplayMode(.inline)
+            .searchable(
+                text: $searchText,
+                placement: .navigationBarDrawer(displayMode: .automatic),
+                prompt: "e.g. Amazon US"
+            )
         }
-        .padding()
     }
 }
 
