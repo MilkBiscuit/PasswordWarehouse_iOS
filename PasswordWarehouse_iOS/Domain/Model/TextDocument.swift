4381a586f2c0527b02e2b3b4365ba85f942fb3eb
Chandler Cheng <dc9909@hotmail.com>
2023-12-01 06:06:43 +1300

    Add TextDocument and ExportFileView

---
+++ PasswordWarehouse_iOS/Domain/Model/TextDocument.swift
@@ -0,0 +1,36 @@
+//
+//
+// TextDocument.swift
+//
+// Created on 1/12/23
+// Copyright © 2023 Chandler Cheng. All rights reserved.
+//
+        
+
+import Foundation
+import SwiftUI
+import UniformTypeIdentifiers
+
+struct TextDocument: FileDocument {
+    static var readableContentTypes: [UTType] {
+        [.plainText]
+    }
+    
+    var text = ""
+    
+    init(text: String) {
+        self.text = text
+    }
+    
+    init(configuration: ReadConfiguration) throws {
+        if let data = configuration.file.regularFileContents {
+            text = String(decoding: data, as: UTF8.self)
+        } else {
+            text = ""
+        }
+    }
+    
+    func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
+        FileWrapper(regularFileWithContents: Data(text.utf8))
+    }
+}
