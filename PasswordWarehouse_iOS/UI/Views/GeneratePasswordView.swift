6f0757d9b6e38e7aabf751b7c762269308f322bb
Chandler Cheng <dc9909@hotmail.com>
2023-12-04 08:59:13 +1300

    Build GeneratePassword UI

--- PasswordWarehouse_iOS/UI/Views/GeneratePasswordView.swift
+++ PasswordWarehouse_iOS/UI/Views/GeneratePasswordView.swift
@@ -10,19 +10,33 @@ import SwiftUI
 struct GeneratePasswordView: View {
     @Environment(\.dismiss) var dismiss
     @State private var sheetHeight: CGFloat = .zero
+    @State private var hasUppercase: Bool = true
+    @State private var hasLowercase: Bool = true
+    @State private var hasNumber: Bool = true
+    @State private var hasSymbol: Bool = false
+    @State private var length = 10
     
     var body: some View {
-        ZStack {
-            Color.green
-            VStack {
-                Image(systemName: "magnifyingglass")
-                    .font(.system(size: 100.0))
-                Button("Press to dismiss") {
-                    dismiss()
-                }
-                .font(.title)
+        VStack {
+            Toggle("Uppercase", isOn: $hasUppercase)
+            Toggle("Lowercase", isOn: $hasLowercase)
+            Toggle("Number", isOn: $hasNumber)
+            Toggle("Symbol", isOn: $hasSymbol)
+            Stepper("Length:  \(length)", value: $length, in: 4...20)
+            Spacer().frame(height: 20)
+            KumaPasswordField(text: .constant("AABBCC"))
+
+            Spacer().frame(height: 20)
+            Button(action: {}) {
+                Label("Generate", systemImage: "arrow.clockwise")
             }
+            Spacer().frame(height: 20)
+            Button(action: { dismiss() }) {
+                Text("Use this password")
+            }
+            .buttonStyle(.borderedProminent)
         }
+        .padding()
         .overlay {
             GeometryReader { geometry in
                 Color.clear.preference(key: InnerHeightPreferenceKey.self, value: geometry.size.height)
